f2f02916093485f07df6abe480bb75445b4be234
Wiktor Sosnowski <wiktorsoswisosnowski@gmail.com>
2026-05-11 17:04:25 +0200

    .

--- radio_controller.py
+++ radio_controller.py
@@ -134,7 +134,7 @@ class RadioController:
                 # before STM32 begins transmitting its TELEM response.
                 sleep(_POST_TX_RX_SETTLE_S)
 
-                frame = self._wait_for_frame()
+                # frame = self._wait_for_frame()
 
                 if False: # if frame is None:
                     last_error = "Timeout: no TELEM received"
